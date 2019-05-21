ebfa86362ac4b637ebc7615d8782e8730fdc32e1
Patria <reika.adaichi@gmail.com>
2019-05-21 15:33:38 +0700

    no message

--- Tugas Pweb PHP form.php
+++ Tugas Pweb PHP form.php
@@ -6,17 +6,23 @@
 </head>
 <body>
 <?php
-$nameErr = $NIMErr = $name = $NIM = "";
+$nameErr = $NIMErr = $name = $NIM = $error = $x = $d = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Silakan Isi Nama";
-  } else {
-    $name = test_input($_POST["name"]);
+  if(strlen($_POST["name"])>10 || strlen($_POST["NIM"])>10){
+    $error="Input Lebih Dari Sepuluh Kurangi";
   }
-  if (empty($_POST["NIM"])) {
-    $NIMErr = "Silakan Isi NIM ";
-  } else {
-    $NIM = test_input($_POST["NIM"]);
+  else{
+    $x="<h2>Selamat Datang</h2>";
+    if (empty($_POST["name"])) {
+      $nameErr = "Silakan Isi Nama";
+    } else {
+      $name = test_input($_POST["name"]);
+    }
+    if (empty($_POST["NIM"])) {
+      $NIMErr = "Silakan Isi NIM ";
+    } else {
+      $NIM = test_input($_POST["NIM"]);
+    }
   }
 }
 function test_input($data) {
@@ -28,19 +34,24 @@ function test_input($data) {
 ?>
 <h2>Tugas Pweb : </h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="name" size="10" value="<?php echo $name;?>">
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
-  NIM: <input type="text" name="NIM" size="10" value="<?php echo $NIM;?>">
+  NIM: <input type="text" name="NIM" value="<?php echo $NIM;?>">
   <span class="error">* <?php echo $NIMErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">
 </form>
 <?php
-echo "<h2>Selamat Datang</h2>";
-echo $name;
-echo "<br>";
-echo $NIM;
+if(!empty($name) && !empty($NIM) ){
+  echo $x;
+  echo $name;
+  echo "<br>";
+  echo $NIM."<br>";
+}
+else{
+  echo $error;
+}
 ?>
 
 </body>
